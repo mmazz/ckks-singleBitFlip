85f91b37bf99897655bb8e9710089ad97b4cdddc
Matias Mazzanti <mazzantimatiass@gmail.com>
2026-01-28 15:09:22 -0300

    Test dnum and scalingTechnique

---
+++ analysis/encode_analysis.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import sys
+import os
+
+sys.path.append(os.path.abspath('./'))
+from utils import config
+
+import matplotlib.pyplot as plt
+from utils.args import parse_args, build_filters
+from utils.io_utils import load_campaign_data, load_and_filter_campaigns
+
+show = config.show
+width = int(config.width)
+
+def plot_bit(stats, Q, Delta, ax=None, label_prefix="", scatter=False):
+
+    if ax is None:
+        ax = plt.gca()
+    x = stats["bit"].to_numpy()
+
+    x_norm = np.empty_like(x, dtype=float)
+
+    mask = x < Delta
+    x_norm[mask] = x[mask] / Delta
+    x_norm[~mask] = 1.0 + (x[~mask] - Delta) / (Q - Delta)
+
+
+    mean = stats["mean_l2"].to_numpy()
+
+    if scatter:
+        ax.scatter(
+            x_norm,
+            mean, color=config.colors["red"],
+            s=80,                # 👈 tamaño del punto
+            zorder=4,            # 👈 arriba del plot
+            label=f"{label_prefix}Mean $L_2$"
+        )
+    else:
+        ax.plot(
+            x_norm,
+            mean,
+            linewidth=2,
+            marker="o",
+            markersize=6,
+            zorder=2,            # 👈 abajo del scatter
+            label=f"{label_prefix}Mean $L_2$"
+        )
+
+    ax.set_yscale("symlog")
+    ax.set_xlabel("Bit index")
+    ax.set_ylabel("$L_2$ norm (symlog)")
+    ax.grid(True, which="both")
+
+def stats_by_bit_uniform_coeff(data, drop_dc=True):
+    if drop_dc:
+        data = data[data["coeff"] != 0]
+    data = data[data["coeff"] != 0]
+    per_coeff = (
+        data
+        .groupby(["bit", "coeff"], as_index=False)
+        .agg(l2_mean=("l2_norm", "mean"))
+    )
+
+    # 2) estadística por bit
+    per_bit = (
+        per_coeff
+        .groupby("bit", as_index=False)
+        .agg(
+            mean_l2=("l2_mean", "mean"),
+            std_l2=("l2_mean", lambda x: x.std(ddof=0)),
+            min_l2=("l2_mean", "min"),
+            max_l2=("l2_mean", "max"),
+            count=("l2_mean", "count"),
+        )
+    )
+    return per_bit
+
+def main():
+    args = parse_args()
+    filters_heaan = build_filters(args)
+    print("Filtros activos:", filters_heaan)
+
+    filters_openfhe = filters_heaan.copy()
+    filters_openfhe["library"] = ("str", "openfhe")
+    filters_openfhe["bitPerCoeff"] = ("int", 64)
+    selected_heaan = load_and_filter_campaigns(config.CAMPAIGNS_CSV, filters_heaan)
+    selected_openfhe = load_and_filter_campaigns(config.CAMPAIGNS_CSV, filters_openfhe)
+
+    if selected_openfhe.empty:
+        raise RuntimeError("No hay campañas que cumplan los filtros")
+    if selected_heaan.empty:
+        raise RuntimeError("No hay campañas que cumplan los filtros")
+
+    data_openfhe = load_campaign_data(selected_openfhe, config.DATA_DIR)
+    data_heaan = load_campaign_data(selected_heaan, config.DATA_DIR)
+    print(data_openfhe.head())
+    print(data_heaan.head())
+    openfhe = stats_by_bit_uniform_coeff(data_openfhe)
+    heaan = stats_by_bit_uniform_coeff(data_heaan)
+    print(openfhe)
+    Q_openfhe = filters_openfhe["logQ"][1]
+    Delta_openfhe = filters_openfhe["logDelta"][1]
+    Q_HEAAN= 2*filters_heaan["logQ"][1]
+    Delta_HEAAN =  filters_heaan["logDelta"][1] + filters_heaan["logQ"][1]
+
+    fig, ax = plt.subplots(figsize=(12, 5))
+
+    plot_bit(openfhe, Q_openfhe, Delta_openfhe, ax=ax, label_prefix=f"openfhe")
+    plot_bit(heaan, Q_HEAAN, Delta_HEAAN, ax=ax, label_prefix=f"heaan")
+    ax.legend()
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
